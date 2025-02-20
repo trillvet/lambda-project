eeba25ab8b415e14d982e6fc8def3fce0253d6db
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-20 14:42:04 -0800

    Last test of the day but it mostly kind of sort of works currently.

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda! Updated via GitHub pipeline. Trying this again. And Again. And Again.'
+        'body': 'Last time testing this today to see if my function is updated after doing a local git-commit on my computer.'
     }
