9d8a97a60e5be72502fadce6f3eac96650480cd5
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-20 12:26:09 -0800

    Testing to see if my GitHub CodePipeline is working

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda! This is updated via GitHub.'
+        'body': 'Hello from Lambda! Updated via GitHub pipeline.'
     }
