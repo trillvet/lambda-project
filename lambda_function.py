99e8e425d735b0527ec577ff868d5234421ea65f
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-20 13:16:39 -0800

    Testing AWS Code Pipeline

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda! Updated via GitHub pipeline.'
+        'body': 'Hello from Lambda! Updated via GitHub pipeline. Trying this again.'
     }
