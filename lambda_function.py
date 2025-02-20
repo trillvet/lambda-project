93099320c65810b57f55b44dc0c76f3dc984407c
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-20 14:11:01 -0800

    Changes for pipeline testing

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Hello from Lambda! Updated via GitHub pipeline. Trying this again.'
+        'body': 'Hello from Lambda! Updated via GitHub pipeline. Trying this again. And Again'
     }
