89b2850359ad53c1c79fb8979cc3464dc00fe4f4
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-21 14:23:24 -0800

    Day 2 of being semi successful, first code push to trigger update.

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Last time testing this today to see if my function is updated after doing a local git-commit on my computer.'
+        'body': 'First attempt today after updating the build spec yaml.'
     }
