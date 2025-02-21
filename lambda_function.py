5b75e858b4045021529c0944c012e526b41d79b7
trillvet <144061920+trillvet@users.noreply.github.com>
2025-02-21 14:43:51 -0800

    Attempt IV

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': 'Third attempt today after updating the build spec yaml. Was it because I was missing the folder in S3? Also made changes to the buildspec.yml for the Artifact names.'
+        'body': 'Fourth Attempt'
     }
